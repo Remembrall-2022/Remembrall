364f82b93d07af79f5a071254c3f494cce6e0a52
yena777 <parkyena01@sookmyung.ac.kr>
2022-08-11 21:21:41 +0900

    이메일 로그인 / 회원가입 화면 구성

--- app/src/main/java/com/example/remembrall/login/LoginActivity.kt
+++ app/src/main/java/com/example/remembrall/login/LoginActivity.kt
@@ -21,48 +21,18 @@ class LoginActivity : AppCompatActivity() {
         binding= ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = Intent(this, MainActivity::class.java)
+        var intent =  Intent(this, SplashActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
 
-        var keyHash = Utility.getKeyHash(this)
-        // 해시 키 찾기
-        Log.e("hash key : ", keyHash)
-
-        // 카카오계정으로 로그인 공통 callback 구성
-        // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                Log.e(TAG, "카카오계정으로 로그인 실패", error)
-            } else if (token != null) {
-                Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
-                startActivity(intent)
-                finish()
-            }
+        binding.btnBack.setOnClickListener{
+            startActivity(intent)
+            finish()
         }
-        binding.btnLoginLogin.setOnClickListener{
-            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this@LoginActivity)) {
-                UserApiClient.instance.loginWithKakaoTalk(this@LoginActivity) { token, error ->
-                    if (error != null) {
-                        Log.e(TAG, "카카오톡으로 로그인 실패", error)
+//
+//        val intent = Intent(this, MainActivity::class.java)
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
 
-                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                            return@loginWithKakaoTalk
-                        }
 
-                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
-                        UserApiClient.instance.loginWithKakaoAccount(this@LoginActivity, callback = callback)
-                    } else if (token != null) {
-                        Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
-                        startActivity(intent)
-                        finish()
-                    }
-                }
-            } else {
-                UserApiClient.instance.loginWithKakaoAccount(this@LoginActivity, callback = callback)
-            }
         }
-    }
+
 }
