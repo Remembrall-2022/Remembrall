5b21243c2c6616befaf80a98a1de59018ab771b3
Yena Park <parkyena01@sookmyung.ac.kr>
2022-08-19 02:23:45 +0900

    Revert "[feat] - map search 구현"

--- app/src/main/java/com/example/remembrall/map/RvMapSearch.kt
+++
@@ -1,9 +0,0 @@
-package com.example.remembrall.map
-
-class RvMapSearch (
-    val place_name: String,
-    val address : String,
-    val place_url : String,
-    val x: Double,
-    val y: Double
-    )
