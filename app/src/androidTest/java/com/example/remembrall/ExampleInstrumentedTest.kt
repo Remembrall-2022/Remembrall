1b5613b6612e0580bd11c97a0a6862968387de6d
yena777 <parkyena01@sookmyung.ac.kr>
2022-08-22 17:24:19 +0900

    feat:> exampleInstrumentedTest 오타 수정 #9

--- app/src/androidTest/java/com/example/remembrall/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/remembrall/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.remebrall", appContext.packageName)
+        assertEquals("com.example.remembrall", appContext.packageName)
     }
 }
