1bd44a1ac6fc66e38df739d9483aa5a68a65b052
songfox00 <songfox00@gmail.com>
2022-09-08 03:26:30 +0900

    diaryList 디자인 수정

--- app/src/main/java/com/example/remembrall/read/ReadDiaryListRecyclerViewData.kt
+++ app/src/main/java/com/example/remembrall/read/ReadDiaryListRecyclerViewData.kt
@@ -3,5 +3,6 @@ package com.example.remembrall.read
 class ReadDiaryListRecyclerViewData (
     val name: String,
     val imgUrl: String,
-    val triplogId: Long
+    val triplogId: Long,
+    val date: String
 )
